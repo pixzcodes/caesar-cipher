1b089676ac530ee0bc20d0371e40adbed3086559
Pixz <tinycactusking@gmail.com>
2022-09-28 18:32:29 -0400

    Add a loop to shift the alphabet by the shift_number param

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # caesar-cipher
-Implementing a ceasar cipher that takes in a string 
+Implementing a caesar cipher that takes in a string 
 and a shift factor
 
 Call function with caesar_cipher()
--- caesarcipher.rb
+++ caesarcipher.rb
@@ -1,3 +1,21 @@
+#Notes
+# First i will create an array with the aplhabet that will be shifted 
+# by the shift number called the shifted_alphabet
+# Then i will compare each position of the shifted_alphabet to the 
+# letters in the string
+# I think if i asign each letter of the alphabet a number 0-25 then i
+# can asign the letters in the string a shifted alphabet number to change to 
+# Need to remember to add a caps check
 
 
-caesar_cipher
+def caesar_cipher (string, shift_value)
+    shifted_alphabet = ('a'..'z').to_a
+    i = 0
+    while i < shift_value do
+        shifted_alphabet.push(shifted_alphabet.shift)
+        i += 1
+    end
+    
+end
+
+caesar_cipher("nothing",2)
